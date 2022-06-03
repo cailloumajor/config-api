9a4c2af642899e8ebcb4c7091a446cca6fda2570
Arnaud Rocher <arnaud.roche3@gmail.com>
2022-06-03 13:51:28 +0000

    feat: implement signals handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use anyhow::Context;
+use async_std::prelude::*;
 use async_std::sync::{Arc, RwLock};
 use clap::Parser;
 
 mod config;
 
 use config::handler as config_handler;
+use futures::future::{AbortHandle, Abortable, Aborted};
+use signal_hook::consts::TERM_SIGNALS;
+use signal_hook::low_level::signal_name;
+use signal_hook_async_std::Signals;
 
 #[derive(Parser)]
 #[clap(version, about)]
@@ -19,9 +24,22 @@ pub struct AppState {
     toml_value: Arc<RwLock<toml::Value>>,
 }
 
+async fn handle_signals(mut signals: Signals, abort_handle: AbortHandle) {
+    while let Some(signal) = signals.next().await {
+        let signame = signal_name(signal).unwrap_or("unknown");
+        eprintln!(r#"signal={} msg="received signal, exiting""#, signame);
+        abort_handle.abort();
+    }
+}
+
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
+
+    let (abort_handle, abort_registration) = AbortHandle::new_pair();
+    let signals = Signals::new(TERM_SIGNALS)?;
+    let signals_handle = signals.handle();
+    let signal_task = async_std::task::spawn(handle_signals(signals, abort_handle));
 
     let mut app = tide::with_state(AppState {
         toml_value: Arc::new(RwLock::new(
@@ -31,9 +49,15 @@ async fn main() -> anyhow::Result<()> {
     });
     app.at("/config/*path").get(config_handler);
     eprintln!(r#"listen="{}" msg="start listening""#, args.listen_address);
-    app.listen(args.listen_address)
-        .await
-        .context("listen error")?;
+
+    let listen_future = Abortable::new(app.listen(args.listen_address), abort_registration);
+    match listen_future.await {
+        Err(Aborted) => Ok(()),
+        Ok(listen_result) => listen_result.context("listen error"),
+    }?;
+
+    signals_handle.close();
+    signal_task.await;
 
     Ok(())
 }
