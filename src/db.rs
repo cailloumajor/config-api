d35c427319495761255cb53a995a1753887a8314
Arnaud Rocher <arnaud.roche3@gmail.com>
2023-09-28 06:49:13 +0000

    fix: implement DRY roundtrip channel

--- src/db.rs
+++ src/db.rs
@@ -6,9 +6,10 @@ use futures_util::TryFutureExt;
 use mongodb::bson::{doc, Bson, Document};
 use mongodb::options::ClientOptions;
 use mongodb::Client;
-use tokio::sync::{mpsc, oneshot};
 use tokio::task::JoinHandle;
 use tracing::{debug, error, info, info_span, instrument, Instrument};
+
+use crate::channel::{roundtrip_channel, RoundtripSender};
 
 const APP_NAME: &str = concat!(env!("CARGO_PKG_NAME"), " (", env!("CARGO_PKG_VERSION"), ")");
 
@@ -23,6 +24,8 @@ pub(crate) struct Config {
     mongodb_database: String,
 }
 
+pub(crate) type HealthChannel = RoundtripSender<(), bool>;
+
 #[derive(Debug)]
 pub(crate) struct GetConfigRequest {
     pub(crate) collection: String,
@@ -36,7 +39,7 @@ pub(crate) enum GetConfigResponse {
     Error,
 }
 
-pub(crate) type GetConfigMessage = (GetConfigRequest, oneshot::Sender<GetConfigResponse>);
+pub(crate) type GetConfigChannel = RoundtripSender<GetConfigRequest, GetConfigResponse>;
 
 #[derive(Clone)]
 pub(crate) struct Database(mongodb::Database);
@@ -55,15 +58,15 @@ impl Database {
         Ok(Self(database))
     }
 
-    pub(crate) fn handle_health(&self) -> (mpsc::Sender<oneshot::Sender<bool>>, JoinHandle<()>) {
-        let (tx, mut rx) = mpsc::channel::<oneshot::Sender<bool>>(1);
+    pub(crate) fn handle_health(&self) -> (HealthChannel, JoinHandle<()>) {
+        let (tx, mut rx) = roundtrip_channel(1);
         let command = doc! { "ping": 1 };
         let cloned_self = self.clone();
 
         let task = tokio::spawn(
             async move {
                 info!(status = "started");
-                while let Some(response_tx) = rx.recv().await {
+                while let Some((_, response_tx)) = rx.recv().await {
                     debug!(msg = "request received");
                     let outcome = cloned_self
                         .0
@@ -82,8 +85,8 @@ impl Database {
         (tx, task)
     }
 
-    pub(crate) fn handle_get_config(&self) -> (mpsc::Sender<GetConfigMessage>, JoinHandle<()>) {
-        let (tx, mut rx) = mpsc::channel::<GetConfigMessage>(5);
+    pub(crate) fn handle_get_config(&self) -> (GetConfigChannel, JoinHandle<()>) {
+        let (tx, mut rx) = roundtrip_channel::<GetConfigRequest, GetConfigResponse>(5);
         let cloned_self = self.clone();
 
         let task = tokio::spawn(
